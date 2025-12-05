2c07e91370d2fd92a146469cdd7d4baf00be1b03
Johannes De Beer <johannes.debeer@ae1impm0y0g5y.omc.oneds.com>
2025-12-05 11:13:03 +0400

    Prep for deploy: socket env and app

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:4000', {
+const url = import.meta.env.VITE_SOCKET_URL || 'http://localhost:4000'
+const socket = io(url, {
   autoConnect: true,
 })
 
